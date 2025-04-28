aa96a17649f1893837966e371dcb9df86da04c45
ygor emmanoel <ygoremmanoel23@gmail.com>
2025-04-28 09:30:39 -0300

    atividade de listas

---
+++ Arquivo01.py
@@ -0,0 +1,3 @@
+arrayCompras=['banana','laranja','maçã']
+for i in range(3):
+    print(arrayCompras[0])
---
+++ Arquivo02.py
@@ -0,0 +1,4 @@
+nome=["","","","",""]
+for i in range(len(nome)):
+    nome[i]= input(f"digite  5 nomes {i+1}: ")
+print(nome)
